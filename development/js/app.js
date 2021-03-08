4b720ada7a7a3b14030d91aaa6df1a8dca8a09b3
albi1018 <albi1018@gmail.com>
2021-03-08 12:06:28 +0100

    add playerVsAi function

--- development/js/app.js
+++ development/js/app.js
@@ -1,4 +1,5 @@
 const pokemons = document.querySelectorAll(".figure");
+const btn = document.querySelector(".start");
 
 const gameScore = {
     numberOfGames: 0,
@@ -12,7 +13,9 @@ const game = {
     aiChoice: "",
 }
 
-function pokemonSelection () {
+let result;
+
+function pokemonSelection() {
     game.playerChoice = this.dataset.option
     console.log(game.playerChoice);
     pokemons.forEach(pokemon => pokemon.style.boxShadow = "");
@@ -25,7 +28,41 @@ function pokemonSelection () {
     }
 }
 
+function aiSelection() {
+    game.aiChoice = Math.floor(Math.random() * 3 + 1)
+    if (game.aiChoice === 1) {
+        game.aiChoice = "charmander";
+    } else if (game.aiChoice === 2) {
+        game.aiChoice = "squirtle"
+    } else {
+        game.aiChoice = "bulbasaur"
+    }
+    console.log(game.aiChoice);
+    return game.aiChoice
+}
 
+function playerVsAi(player, ai) {
+    if (player === "charmander" && ai === "bulbasaur" || player === "bulbasaur" && ai === "squirtle"
+        || player === "squirtle" && ai === "charmander") {
+        console.log("wygrałeś!");
+    } else if (player === "bulbasaur" && ai === "charmander" || player === "squirtle" && ai === "bulbasaur"
+        || player === "charmander" && ai === "squirtle") {
+        console.log("przegrałeś!")
+        } else {
+    console.log("remis!");}
+}
+
+function start() {
+    if (game.playerChoice === "") {
+        return alert("Wybierz pokemona!")
+    }
+    aiSelection();
+    playerVsAi(game.playerChoice, game.aiChoice)
+}
+
+
+
+btn.addEventListener("click", start);
 
 pokemons.forEach(pokemon => pokemon.addEventListener("click", pokemonSelection));
 
